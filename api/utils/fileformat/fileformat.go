558ce56fa419325c1c216ba6bc599c9da96348a0
sajicode <sajioloye@gmail.com>
2020-03-12 01:13:57 +0100

    setup file & error formatters

---
+++ api/utils/fileformat/fileformat.go
@@ -0,0 +1,20 @@
+package fileformat
+
+import (
+	"path"
+	"strings"
+
+	"github.com/twinj/uuid"
+)
+
+// UniqueFormat - format image name to be unique
+func UniqueFormat(fn string) string {
+	//path.Ext() get the extension of the file
+	fileName := strings.TrimSuffix(fn, path.Ext(fn))
+	extension := path.Ext(fn)
+	u := uuid.NewV4()
+	newFileName := fileName + "-" + u.String() + extension
+
+	return newFileName
+
+}
